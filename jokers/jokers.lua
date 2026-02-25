3bb2dd435e7fd1a3fc4889d176d20af139aadfbf
Cartola09 <118937368+Cartola09@users.noreply.github.com>
2026-02-25 00:43:54 -0300

    fix Miner

--- jokers/jokers.lua
+++ jokers/jokers.lua
@@ -572,7 +572,6 @@ SMODS.Joker{
     calculate = function(self, card, context)
         if context.joker_main and card.ability.extra.x_mult > 1 then
             return {
-                message = localize{type='variable', key='a_xmult', vars={card.ability.extra.x_mult}},
                 x_mult = card.ability.extra.x_mult,
                 colour = G.C.MULT
             }
