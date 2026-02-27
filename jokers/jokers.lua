3dd5898434b80fb98661b60a427aa016ea7cbf0b
Cartola09 <118937368+Cartola09@users.noreply.github.com>
2026-02-27 16:36:05 -0300

    fix

--- jokers/jokers.lua
+++ jokers/jokers.lua
@@ -1202,32 +1202,28 @@ function Card.start_dissolve(self, dissolve_colours, shelf_live, item_type)
                 end
             end
 
-            -- Reaper ---
+            -- --- Reaper ---
             if self.config.center.set == 'Joker' then
                 if v.config.center.key == 'j_cd_reaper' and v ~= self then
                     if (self.destroyed or self.getting_sliced or item_type == nil) and not self.selling and item_type ~= true then
-                        v.ability.extra.chips = v.ability.extra.chips + v.ability.extra.gain
-
-                        local card_to_upgrade = v
-
-                        G.E_MANAGER:add_event(Event({
-                            func = function()
-                                card_to_upgrade:juice_up()
-
-                                attention_text({
-                                    text = localize("k_upgrade_ex"),
-                                    colour = G.C.CHIPS,
-                                    scale = 0.6, 
-                                    hold = 0.8,
-                                    major = card_to_upgrade
-                                })
-                                play_sound("chips1", 1, 1.2)
-                                return true
-                            end
-                        }))
-                    end
+            
+                    v.ability.extra.chips = v.ability.extra.chips + v.ability.extra.gain
+                    local card_to_upgrade = v
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            card_to_upgrade:juice_up()
+                            attention_text({
+                                text = localize("k_upgrade_ex"),
+                                colour = G.C.CHIPS,
+                                scale = 0.6, 
+                                hold = 0.8, 
+                                major = card_to_upgrade
+                            })
+                            play_sound("chips1", 1, 1.2)
+                            return true
+                        end
+                    }))
                 end
-
             end
         end
 
